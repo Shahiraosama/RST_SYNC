0a043a2b978ec775e1492c654833137a7dcd065f
Shahiraosama <120308697+Shahiraosama@users.noreply.github.com>
2023-09-28 01:20:49 +0300

    Add files via upload

---
+++ RST_SYNC.v
@@ -0,0 +1,33 @@
+module RST_SYNC #(parameter STAGES = 2)(
+
+input	wire	CLK,
+input	wire	RST,
+output	wire	SYNC_RST
+
+);
+
+reg	[STAGES-1:0]	FF;
+
+always@(posedge CLK or negedge RST)
+begin
+
+if(!RST)
+begin
+
+FF <= 'b0;
+
+end
+
+else
+begin
+
+FF = { FF[STAGES-2:0] , 1}; 
+
+
+end
+
+end
+
+
+assign SYNC_RST = FF[STAGES-1] ;
+endmodule
